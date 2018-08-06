59511bb765aaba70c1601810e6ed0b53341db71e
Damian Marut <damianmarut1@gmail.com>
2018-08-06 15:02:07 +0200

    add root route, index view and partials, basic navbar

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-var express = require("express");
+// MODULES REQUIREMENTS
+var express = require("express"),
+	bodyParser = require("body-parser"),
+	methodOverride = require("method-override");
 
+// MODELS
+
+// ROUTES REQUIREMENTS
+var indexRoutes = require("./routes/index");
+
+// APP SETTINGS
 var app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+
+// ROUTES
+app.use("/", indexRoutes);
+
+
+
+app.listen(3000, "localhost", function(){
+	console.log("Server is listening!");
+});
